a442101d34c47a9d0be587a4b233862b3701f2b7
Solpostgit <begemot_off@mail.ru>
2023-08-27 17:05:39 +0300

    Added task1-4

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();/*
+        task2();
         task3();
-        task4();
+        task4();/*
         task5();*/
     }
 
@@ -13,7 +13,7 @@ public class Main {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[3];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100000) + 100000;
+            arr[i] = random.nextInt(100) + 100000;
         }
         return arr;
     }
@@ -24,8 +24,8 @@ public class Main {
         int sum = 0;
         for (int element : arr) {
             sum += element;
-            System.out.println(Arrays.toString(arr));
         }
+        System.out.println(Arrays.toString(arr));
         System.out.println(sum);
         System.out.println();
     }
@@ -33,12 +33,43 @@ public class Main {
     public static void task2() {
         System.out.println("Task 2");
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        for (int i : arr) {
+        for (int i = 0; i < arr.length; i++) {
             if (i == arr.length - 1) {
                 System.out.println(arr[i]);
+                System.out.println();
                 break;
             }
-           // System.out.println(arr[i]);
+            System.out.print(arr[i] + ", ");
         }
+    }
+
+    public static void task3() { //Вывод массива через запятую используя метод Arrays.toString
+        System.out.println("Task 3");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void task4() { //Сравнение массивов
+        System.out.println("Task 4");
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3, 4, 5};
+        Boolean arrAreEqual = true;
+        arrAreEqual = arr.length == arr2.length;
+        if (arrAreEqual) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != arr2[i]) {
+                    arrAreEqual = false;
+                }
+            }
+            if (arrAreEqual) {
+                System.out.println("Массивы одинаковые");
+            } else {
+                System.out.println("Массивы разные");
+            }
+        } else {
+            System.out.println("Массивы разной длины");
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
     }
 }
